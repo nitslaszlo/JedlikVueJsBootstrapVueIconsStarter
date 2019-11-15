892a1fea497957fc83dab68fc4c52932fdc16651
László Nits <nitslaszlo@gmail.com>
2019-11-15 18:54:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hello World!" />
+    <HelloWorldPlus />
+    <FontAwesomeIcons />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorldPlus from "./components/HelloWorldPlus.vue";
+import FontAwesomeIcons from "./components/FontAwesomeIcons.vue";
+
+@Component({
+  components: {
+    HelloWorld,
+    HelloWorldPlus,
+    FontAwesomeIcons
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+</style>
